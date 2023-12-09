8e7e85957b21573e8e9bda775e259cbd25653979
Mikhail Saburov <saburov1985@gmail.com>
2023-12-09 21:39:54 +1100

    добавил код

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import Container from "@mui/material/Container";
+import Container from '@mui/material/Container';
 
-import { Header } from "./components";
-import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+import { Header } from './components';
+import { Home, FullPost, Registration, AddPost, Login } from './pages';
 
 function App() {
   return (
     <>
       <Header />
       <Container maxWidth="lg">
-        <Home />
+        {/*<Home /> */}
         {/*<FullPost />*/}
         {/*<AddPost />*/}
         {/*<Login />*/}
