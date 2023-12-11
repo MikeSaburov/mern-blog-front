2b1962e20483bb10710698aafb32d1ea9a7bf4f5
Mikhail Saburov <saburov1985@gmail.com>
2023-12-11 23:08:32 +1100

    refactor(Header): прокинул функцию выхода

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,12 +4,17 @@ import Button from '@mui/material/Button';
 import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectIsAuth } from '../../redux/slices/auth';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectIsAuth } from '../../redux/slices/auth';
 export const Header = () => {
   const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch();
 
-  const onClickLogout = () => {};
+  const onClickLogout = () => {
+    if (window.confirm('Вы действительно хотите выйти?')) {
+      dispatch(logout());
+    }
+  };
 
   return (
     <div className={styles.root}>
